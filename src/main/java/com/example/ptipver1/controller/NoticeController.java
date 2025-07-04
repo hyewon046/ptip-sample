6fb5f3727e6e28e3dd717411b7eef61946458c0b
phw <gpdjsl1216@gmail.com>
2025-07-04 17:19:00 +0900

    1

--- src/main/java/com/example/ptipver1/controller/NoticeController.java
+++ src/main/java/com/example/ptipver1/controller/NoticeController.java
@@ -2,6 +2,7 @@ package com.example.ptipver1.controller;
 
 import com.example.ptipver1.dto.NoticeDto;
 import com.example.ptipver1.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import java.util.List;
 @Controller
 public class NoticeController {
 
+    @Autowired
     private  NoticeService noticeService;
 
     @GetMapping("/notices")
--- src/main/java/com/example/ptipver1/crawler/NoticeCrawler.java
+++ src/main/java/com/example/ptipver1/crawler/NoticeCrawler.java
@@ -11,13 +11,14 @@ import java.util.List;
 
 @Component
 public class NoticeCrawler implements SiteCrawler {
+
     @Override
     public List<NoticeDto> fetchNotices() {
         List<NoticeDto> notices = new ArrayList<>();
         try {
-            Document doc = Jsoup.connect("https://www.ptu.ac.kr/bbs/www/310/32031/artclView.do").get();
+            Document doc = Jsoup.connect("https://www.ptu.ac.kr/bbs/www/310/32031/artclView.do").get(); //32031부분이 pk
             System.out.println(doc.title());
-            Elements items = doc.select(".notice-list li a");
+            Elements items = doc.select(".notice-list li a"); //이 부분을 어떤 태그로 넣어야할지 모루겠어요,,,,,
 
             for (var element : items) {
                 String url = element.absUrl("href");
